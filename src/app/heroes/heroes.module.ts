31e823a42926dcb9d8894ffe843203791aca08b3
Yhomarth Reyes <yhomarth@gmail.com>
2022-01-09 20:15:35 -0400

    Clase 185 completada, Material Sidenav, Toolbar e iconos

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -7,6 +7,7 @@ import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
+import { MaterialModule } from '../material/material.module';
 
 
 
@@ -21,6 +22,7 @@ import { ListadoComponent } from './pages/listado/listado.component';
   imports: [
     CommonModule, 
     FlexLayoutModule,
+    MaterialModule,
     HeroesRoutingModule
   ]
 })
