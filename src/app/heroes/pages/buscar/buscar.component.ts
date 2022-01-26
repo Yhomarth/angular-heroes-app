656efa6c27c135970dad5db3bca194071fe00bbc
Yhomarth Reyes <yhomarth@gmail.com>
2022-01-25 22:04:19 -0400

    Clase 199 completada, AutoComplete - Segunda Parte

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 import { Heroe } from '../../interfaces/heroe.interface';
 import { HeroesService } from '../../services/heroes.service';
 
@@ -13,6 +14,9 @@ export class BuscarComponent implements OnInit {
 
   termino : string = '';
   heroes : Heroe[] = [];
+  heroeSeleccionado !: Heroe;
+
+
 
   constructor(private heroeService : HeroesService) { }
 
@@ -20,8 +24,18 @@ export class BuscarComponent implements OnInit {
   }
 
   buscarHeroe(){
-    this.heroeService.getHeroes()
-      .subscribe(  heroes => this.heroes = heroes  )
+    this.heroeService.getSugerencias( this.termino )
+      .subscribe(  heroes => this.heroes = heroes  );
+  }
+
+  opcionSeleccionada(event: MatAutocompleteSelectedEvent){
+
+    const  heroe: Heroe = event.option.value;
+
+    this.heroeService.getHeroePorId( heroe.id! )
+        .subscribe(  heroe => this.heroeSeleccionado = heroe  );
+        
+    this.termino = heroe.superhero;
   }
 
 }
