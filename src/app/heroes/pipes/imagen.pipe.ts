2ddaa0a6d818620db1804219d54fcf7745194149
Yhomarth Reyes <yhomarth@gmail.com>
2022-02-08 19:50:26 -0400

    Clase 212 completada, Pipes Puros e impuros

--- src/app/heroes/pipes/imagen.pipe.ts
+++ src/app/heroes/pipes/imagen.pipe.ts
@@ -4,11 +4,14 @@ import { Heroe } from '../interfaces/heroe.interface';
 @Pipe(
      {
          name : 'imagen'
+         // pure: false
      }
 )
 export class ImagenPipe implements PipeTransform {
 
     transform(heroe: Heroe) : string {
+
+        console.log('cambio realizado');
 
         if(!heroe.id || !heroe.alt_img){
             return 'assets/no-image.png';
